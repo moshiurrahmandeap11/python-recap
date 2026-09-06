df4f2d71359ac33b2a8584feb10573bb26721155
moshiurrahmandeap11 <moshiurrahmandeap@gmail.com>
2026-09-07 02:17:45 +0600

    Add while loop example to print numbers from 10 to 1

---
+++ while_loops.py
@@ -0,0 +1,9 @@
+# this is a whle loop example which is used to print numbers from 10 to 1 in reverse order and remember that the loop is a iteration variable which is used to control the loop and it is initialized with a value of 10 and the loop will continue until the value of the loop variable is greater than or equal to 1 and in each iteration the value of the loop variable will be decremented by 1 and the value of the loop variable will be printed in each iteration.
+
+
+number = 10
+
+while number > 0:
+    print(number)
+    number = number - 1
+print("Blast off!")
