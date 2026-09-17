7fc002cdbf669252d30f57c274f474dd136f108b
moshiurrahmandeap11 <moshiurrahmandeap@gmail.com>
2026-09-17 22:01:04 +0600

    Add basic calculator and CGPA calculator implementations

---
+++ basic_calculator.py
@@ -0,0 +1,2 @@
+# today we gonna build a basic calculator using python. This calculator will be able to perform basic arithmetic operations like addition, subtraction, multiplication, division and remainder.
+
---
+++ cgpa_calculator.py
@@ -0,0 +1,5 @@
+#this is a simple CGPA calculator
+
+x = int(input("Enter the number of subjects: "))
+total = 0
+
